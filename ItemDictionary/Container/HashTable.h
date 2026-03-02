3821a6370d091194872c6b5a94e7eec6c79b5742
hsryu2 <thryu9376@gmail.com>
2026-03-03 08:52:10 +0900

    해시 테이블 구현 시작

---
+++ ItemDictionary/Container/HashTable.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Pair.h"
+
+class HashTable
+{
+
+};
---
+++ ItemDictionary/Container/Pair.h
@@ -0,0 +1,50 @@
+#pragma once
+
+template<typename KeyType, typename ValueType>
+class Pair
+{
+public:
+
+	KeyType key;
+
+	ValueType value;
+
+
+	Pair()
+		:key(key), value(value)
+	{
+	}
+
+	Pair(const KeyType& key, const ValueType& value)
+		:key(key), value(value)
+	{
+	}
+
+
+
+	bool operator==(const Pair<KeyType, ValueType>& other)
+	{
+		return key == other.key && value == other.value;
+	}
+	bool operator!=(const Pair<KeyType, ValueType>&other)
+	{
+		return key != other.key && value != other.value;
+	}
+	bool operator>(const Pair<KeyType, ValueType>& other)
+	{
+		return key > other.key && value > other.value;
+	}
+	bool operator<(const Pair<KeyType, ValueType>& other)
+	{
+		return key < other.key && value < other.value;
+	}
+	bool operator>=(const Pair<KeyType, ValueType>& other)
+	{
+		return key >= other.key && value >= other.value;
+	}
+	bool operator<=(const Pair<KeyType, ValueType>& other)
+	{
+		return key <= other.key && value <= other.value;
+	}
+
+};
